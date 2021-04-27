36f9f6d351e22ee1e9ff19365dfc2cb50ed41949
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-04-26 22:32:31 -0400

    Update LinkedList.sol

--- temp/LinkedList.sol
+++ temp/LinkedList.sol
@@ -1,96 +1,181 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >= 0.7 .0 < 0.9 .0;
 
-//assume ascending order for now...
 //reserved index values 0,1
- 
+
 contract LinkedList {
 
-struct node{
+  struct node {
     uint previousIndex;
     uint value;
-}
+  }
 
-mapping(uint=>node) private linkedList;
-uint private currentIndex = 0;
+  mapping(uint => node) private linkedList;
+  uint private currentIndex = 0;
+  bool private asc = true; //false for descending
 
-
-function getCurrentIndex() public view returns (uint) {
-    return currentIndex;
-}
-
-function setCurrentIndex(uint num) public returns(bool){
-    currentIndex = num;
+  function setAsc(bool isAscending) public returns(bool) {
+    asc = isAscending;
     return true;
-}
+  }
 
-//----------------------------------------------------
-function getValueAt(uint index) external view returns(uint){
-    return linkedList[index].value;
-}
-
-function setValueAt(uint index, uint num) public returns(bool){
+  function putNode(uint index, uint previousIndex, uint num) public returns(bool) {
+    linkedList[index].previousIndex = previousIndex;
     linkedList[index].value = num;
     return true;
-}
+  }
 
-function getPreviousIndexAt(uint index) external view returns(uint){
-    return linkedList[index].previousIndex;
-}
-
-function setPreviousIndexAt(uint index, uint num) public returns(bool){
-    linkedList[index].previousIndex = num;
-    return true;
-}
-
-//----------------------------------------------------
-function push(uint index, uint num) public returns(bool){
-    linkedList[index].previousIndex = currentIndex;
-    linkedList[index].value = num;
+  function push(uint index, uint num) public returns(bool) {
+    require(!((asc && index <= currentIndex) || (!asc && index >= currentIndex && currentIndex != 0)));
+    putNode(index, currentIndex, num);
     currentIndex = index;
     return true;
-}
+  }
 
-function pop() public returns(bool){
+  function pop() public returns(bool) {
+    require(currentIndex != 0);
     currentIndex = linkedList[currentIndex].previousIndex;
     return true;
-}
-//----------------------------------------------------
-function insertAt(uint searchIndex, uint index, uint num, bool asc) public returns(bool){
-uint closestIndex = asc? findClosestIndexAsc(searchIndex):findClosestIndexDesc(searchIndex);
-linkedList[index].previousIndex = linkedList[closestIndex].previousIndex;
-linkedList[index].value = num;
-linkedList[closestIndex].previousIndex = index;
-return true;
-}
+  }
 
-function truncateAt(uint searchIndex, bool asc) public returns(bool){
-uint closestIndex = asc? findClosestIndexAsc(searchIndex):findClosestIndexDesc(searchIndex);
-currentIndex = closestIndex;
-return true;
-}
+  function insert(uint index, uint num) public returns(bool) {
+    require(!isIndexInChain(index) && currentIndex != index);
 
-//assumes ascending order
-//returns 1 if not found
-function findClosestIndexAsc(uint searchIndex) public view returns(uint){
-    uint tmpIndex = currentIndex;
-    while(searchIndex<tmpIndex){
-        tmpIndex = linkedList[tmpIndex].previousIndex;
+    if (currentIndex == 0)
+      push(index, num);
+
+    else if (asc)
+      insertAtAsc(index, num);
+
+    else
+      insertAtDesc(index, num);
+
+    return true;
+  }
+
+  function insertAtDesc(uint index, uint num) private returns(bool) {
+    require(!asc);
+    if (currentIndex > index) {
+      push(index, num);
+      return true;
     }
-    return tmpIndex;
-}
 
+    uint closestIndex = findClosestIndexDesc(index);
+    require(closestIndex != 1);
+    if (closestIndex == 0)
+      closestIndex = findPointerIndex(closestIndex);
+    require(closestIndex != 1);
+    putNode(index, linkedList[closestIndex].previousIndex, num);
+    linkedList[closestIndex].previousIndex = index;
+    return true;
+  }
 
-//assumes descending order
-//returns 1 if not found
-function findClosestIndexDesc(uint searchIndex) public view returns(uint){
-    uint tmpIndex = currentIndex;
-    while(searchIndex>tmpIndex){
-        tmpIndex = linkedList[tmpIndex].previousIndex;
+  function insertAtAsc(uint index, uint num) public returns(bool) {
+    require(asc);
+
+    if (currentIndex < index) {
+      push(index, num);
+      return true;
     }
-    return tmpIndex;
-}
 
+    uint closestIndex = findClosestIndexAsc(index);
+    require(closestIndex != 1);
+    putNode(index, linkedList[closestIndex].previousIndex, num);
+    linkedList[closestIndex].previousIndex = index;
+
+    return true;
+  }
+
+  function remove(uint index) public returns(bool) {
+
+    require(!(index == 0 || currentIndex == 0 || !isIndexInChain(index)));
+
+    if (currentIndex == index) {
+      pop();
+      return true;
+    }
+    uint pointerIndex = findPointerIndex(index);
+    require(pointerIndex != 1);
+
+    if (pointerIndex != 0 && linkedList[pointerIndex].previousIndex != 0)
+      linkedList[pointerIndex].previousIndex = linkedList[index].previousIndex;
+
+    if (linkedList[pointerIndex].previousIndex == 0)
+      return true;
+
+    pointerIndex = findPointerIndex(pointerIndex);
+    require(pointerIndex != 1);
+    linkedList[pointerIndex].previousIndex = 0;
+    return true;
+  }
+
+  function findPointerIndex(uint index) public view returns(uint) {
+    uint atCurrent = currentIndex;
+    uint prev = linkedList[atCurrent].previousIndex;
+    uint count = 0;
+    while (prev != index && prev != 0) {
+      if (count >= 10000)
+        return 1;
+      atCurrent = prev;
+      prev = linkedList[atCurrent].previousIndex;
+      ++count;
+    }
+    return atCurrent;
+  }
+
+  function truncateAt(uint index) public returns(bool) {
+    require(index != 0 && index != currentIndex && isIndexInChain(index));
+    currentIndex = index;
+    return true;
+  }
+
+  function isIndexInChain(uint index) public view returns(bool) {
+    if (currentIndex == 0 && index != currentIndex) return false;
+    if (currentIndex == index) return true;
+    uint atCurrent = currentIndex;
+    uint prev = linkedList[atCurrent].previousIndex;
+    uint count = 0;
+    while (prev != 0) {
+      if (count >= 10000)
+        break;
+      atCurrent = prev;
+      prev = linkedList[atCurrent].previousIndex;
+      if (prev == index || atCurrent == index)
+        return true;
+      count++;
+    }
+    return false;
+  }
+
+  function findClosestIndexAsc(uint searchIndex) private view returns(uint) {
+    uint atCurrent = currentIndex;
+    uint prev = linkedList[atCurrent].previousIndex;
+    uint count = 0;
+    while (prev > searchIndex) {
+      if (count >= 10000)
+        return 1;
+      atCurrent = prev;
+      prev = linkedList[atCurrent].previousIndex;
+      ++count;
+    }
+    return atCurrent;
+  }
+
+  function findClosestIndexDesc(uint searchIndex) private view returns(uint) {
+    uint atCurrent = currentIndex;
+    uint prev = linkedList[atCurrent].previousIndex;
+    uint count = 0;
+    while (prev < searchIndex) {
+      if (count >= 10000)
+        return 1;
+      atCurrent = prev;
+      if (atCurrent == 0)
+        return 0;
+      prev = linkedList[atCurrent].previousIndex;
+      ++count;
+    }
+    return atCurrent;
+  }
 
 }
