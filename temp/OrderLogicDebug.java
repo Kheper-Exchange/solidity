cb427bf1e2520a7a7d93f348c67497891c8c77fa
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-04-27 00:08:17 -0400

    Update OrderLogicDebug.java

--- temp/OrderLogicDebug.java
+++ temp/OrderLogicDebug.java
@@ -15,24 +15,34 @@ public class OrderLogicDebug {
 
     public void placeSellOrder(int price, int amount){
         int isInSell = sellOrders.isIndexInChain(price), isInBuy = buyOrders.isIndexInChain(price);
-        if (isInBuy != 2 && isInSell != 2) //required
-			if (sellOrders.getCurrentIndex() == 0 && isInBuy != 1)
-				sellOrders.insert(price, amount);
-			else if (isInSell == 1 && isInBuy != 1)
-				sellOrders.addValue(price, amount);
-			else if ((isInSell == 1 || isInBuy != 1) && isInSell != 1 && isInBuy != 1)
-				sellOrders.insert(price, amount);
+        if(isInBuy==2 || isInSell==2) return; //unreachable
+
+
+        if (sellOrders.getCurrentIndex() == 0 && isInBuy != 1) {
+			sellOrders.insert(price, amount);
+		} else if (isInSell == 1 && isInBuy != 1) {
+			sellOrders.addValue(price, amount);
+		} else if (isInSell != 1 && isInBuy == 1) {
+			System.out.println("process inBuy, get difference, truncate or subtract, place inSell");
+		} else if (isInSell != 1 && isInBuy != 1) {
+			sellOrders.insert(price, amount);
+		}
     }
 
     public void placeBuyOrder(int price, int amount){
         int isInSell = sellOrders.isIndexInChain(price), isInBuy = buyOrders.isIndexInChain(price);
-        if (isInBuy != 2 && isInSell != 2) //required
-			if (buyOrders.getCurrentIndex() == 0 && isInSell != 1)
-				buyOrders.insert(price, amount);
-			else if (isInBuy == 1 && isInSell != 1)
-				buyOrders.addValue(price, amount);
-			else if ((isInBuy == 1 || isInSell != 1) && isInBuy != 1 && isInSell != 1)
-				buyOrders.insert(price, amount);
+        if(isInBuy==2 || isInSell==2) return; //unreachable
+
+
+        if (buyOrders.getCurrentIndex() == 0 && isInSell != 1) {
+			buyOrders.insert(price, amount);
+		} else if (isInBuy == 1 && isInSell != 1) {
+			buyOrders.addValue(price, amount);
+		} else if (isInBuy != 1 && isInSell == 1) {
+			System.out.println("process inBuy, get difference, truncate or subtract, place inSell");
+		} else if (isInBuy != 1 && isInSell != 1) {
+			buyOrders.insert(price, amount);
+		}
     }
 
 }
