ec5ea2c7b470463a9814a5bcfb222a39007ffc54
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-04-27 00:06:44 -0400

    Update OrderLogicDebug.java

--- temp/OrderLogicDebug.java
+++ temp/OrderLogicDebug.java
@@ -9,53 +9,30 @@ public class OrderLogicDebug {
     }
 
     public int getPriceGap(){
-        int sellIndex = sellOrders.getCurrentIndex();
-        int buyIndex = buyOrders.getCurrentIndex();
-        return sellIndex>=buyIndex?sellIndex-buyIndex:buyIndex-sellIndex;
+        int sellIndex = sellOrders.getCurrentIndex(), buyIndex = buyOrders.getCurrentIndex();
+        return sellIndex < buyIndex ? buyIndex - sellIndex : sellIndex - buyIndex;
     }
 
     public void placeSellOrder(int price, int amount){
-        int isInSell = sellOrders.isIndexInChain(price);
-        int isInBuy = buyOrders.isIndexInChain(price);
-        if(isInBuy==2 || isInSell==2) return; //unreachable
-
-
-        if(sellOrders.getCurrentIndex()==0 && isInBuy!=1){
-            sellOrders.insert(price, amount);
-            return;
-        }
-
-        if(isInSell==1 && isInBuy!=1){
-            sellOrders.addValue(price, amount);
-        }
-        else if(isInSell!=1 && isInBuy==1){
-            //process inBuy, get difference, truncate or subtract, place inSell
-        }
-        else if(isInSell!=1 && isInBuy!=1){
-            sellOrders.insert(price, amount);
-        }
+        int isInSell = sellOrders.isIndexInChain(price), isInBuy = buyOrders.isIndexInChain(price);
+        if (isInBuy != 2 && isInSell != 2) //required
+			if (sellOrders.getCurrentIndex() == 0 && isInBuy != 1)
+				sellOrders.insert(price, amount);
+			else if (isInSell == 1 && isInBuy != 1)
+				sellOrders.addValue(price, amount);
+			else if ((isInSell == 1 || isInBuy != 1) && isInSell != 1 && isInBuy != 1)
+				sellOrders.insert(price, amount);
     }
 
     public void placeBuyOrder(int price, int amount){
-        int isInSell = sellOrders.isIndexInChain(price);
-        int isInBuy = buyOrders.isIndexInChain(price);
-        if(isInBuy==2 || isInSell==2) return; //unreachable
-
-
-        if(buyOrders.getCurrentIndex()==0 && isInSell!=1){
-            buyOrders.insert(price, amount);
-            return;
-        }
-
-        if(isInBuy==1 && isInSell!=1){
-            buyOrders.addValue(price, amount);
-        }
-        else if(isInBuy!=1 && isInSell==1){
-            //process inBuy, get difference, truncate or subtract, place inSell
-        }
-        else if(isInBuy!=1 && isInSell!=1){
-            buyOrders.insert(price, amount);
-        }
+        int isInSell = sellOrders.isIndexInChain(price), isInBuy = buyOrders.isIndexInChain(price);
+        if (isInBuy != 2 && isInSell != 2) //required
+			if (buyOrders.getCurrentIndex() == 0 && isInSell != 1)
+				buyOrders.insert(price, amount);
+			else if (isInBuy == 1 && isInSell != 1)
+				buyOrders.addValue(price, amount);
+			else if ((isInBuy == 1 || isInSell != 1) && isInBuy != 1 && isInSell != 1)
+				buyOrders.insert(price, amount);
     }
 
 }
