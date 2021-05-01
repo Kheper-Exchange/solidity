a1b5f22cb422ea0da92ef8e687c0074de702f565
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-01 19:33:36 -0400

    Update TradingNodeDebug.java

--- temp/TradingNodeDebug.java
+++ temp/TradingNodeDebug.java
@@ -1,55 +1,90 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+
+
 public class TradingNodeDebug {
 
     private static int lowestId;
-    private static int currentid;
+    private static int currentId;
 
     private static Map<Integer, Bid> statusMap = new HashMap<>();
     private static Map<String, Integer> withdrawnAmount = new HashMap<>();
 
-    public static void ask(String address, int amount){
+    public static boolean placeAsk(String usrAddress, int amount){
         Bid bid = new Bid();
-        bid.address = address;
+        bid.address = usrAddress;
         bid.amount = amount;
-        statusMap.put(currentid,bid);
-        ++currentid;
+        statusMap.put(currentId,bid);
+        ++currentId;
+        return true;
     }
+
+    public static boolean answer(int amount){
+        if(!(amount>0)) return false; //require
+        int totalamt = getTotalAmount();
+
+        if (!(amount <= totalamt)) return false; //require
+        if(!((totalamt-amount)>(totalamt/100))) return false; //require
+
+        if (amount == totalamt) {
+                lowestId = currentId;
+            } else {
+                int res0 = findId(amount);
+                int res1 = findAmount(amount);
+                if(res0==0 && res1==0) return false; //require
+                lowestId = res0;
+                statusMap.get(res0).amount -= res1;
+            }
+        return true;
+    }
+
+    //-----------------------------------------------------------
 
     public static int getTotalAmount(){
         int ret = 0;
-        for(int y=lowestId;y<currentid;++y)
-			ret += statusMap.get(y).amount;
+        for(int y = lowestId; y< currentId; ++y) {
+            if(y==10000) return 0;
+            ret += statusMap.get(y).amount;
+        }
         return ret;
     }
 
-    public static void answer(String address, int amount){
-        int totalamt = getTotalAmount();
-        if (amount <= totalamt)
-			if (amount == totalamt)
-				lowestId = currentid;
-			else {
-				int[] res = findId(amount);
-				if(res[0]==0 && res[1]==1) return;
-				lowestId = res[0];
-				statusMap.get(res[0]).amount -= res[1];
-			}
-    }
 
-    private static int[] findId(int amount){
+
+    private static int findId(int amount){
         int t=lowestId;
-        while (t<currentid) {
+        while (t< currentId) {
+            if(t==10000) return 0; //not found
 			int amtAt = statusMap.get(t).amount;
 			if (amtAt == amount)
-				return new int[] { t++, amount };
+				return t++;
 			if (amtAt > amount)
-				return new int[] { t, amount };
+				return t;
 			amount -= amtAt;
             ++t;
         }
-        return new int[]{0,0}; //not found
+        return 0; //not found
     }
+
+    private static int findAmount(int amount){
+        int t=lowestId;
+        while (t< currentId) {
+            if(t==10000) return 0; //not found
+            int amtAt = statusMap.get(t).amount;
+            if (amtAt == amount)
+                return amount;
+            if (amtAt > amount)
+                return amount;
+            amount -= amtAt;
+            ++t;
+        }
+        return 0; //not found
+    }
+
+
 
     private static int findAllowedAmount(String address){
         int ret = 0;
@@ -64,16 +99,19 @@ public class TradingNodeDebug {
         return ret;
     }
 
-    private static void withdrawAll(String address){
-        int amt = findAllowedAmount(address);
-        withdrawnAmount.put(address, !withdrawnAmount.containsKey(address) ? amt : withdrawnAmount.get(address) + amt);
+    private static boolean withdrawAll(String usrAddress){
+        int amt = findAllowedAmount(usrAddress);
+        if(amt!=0) {
+            withdrawnAmount.put(usrAddress, withdrawnAmount.get(usrAddress)+amt);
+        }
+        return true;
     }
 
-
+//-----------------------------------------------------------
     public static String asString(){
         String ret = "";
         int t=lowestId;
-        while (t<currentid) {
+        while (t< currentId) {
             ret += "[" + t + ":" + statusMap.get(t).address + ":" + statusMap.get(t).amount + "]";
             ++t;
         }
@@ -83,19 +121,19 @@ public class TradingNodeDebug {
 
     public static void main(String[] args){
         String addressA = "x00000A", addressB = "x00000B", addressC = "x00000C", addressD = "x00000D";
-        ask(addressA, 213);
-        ask(addressB, 2);
-        ask(addressC, 5);
+        placeAsk(addressA, 213);
+        placeAsk(addressB, 2);
+        placeAsk(addressC, 5);
 
-        answer(addressD,220);
+        answer(220);
 
         System.out.println(asString());
-        ask(addressA, 213);
-        ask(addressB, 2);
-        ask(addressC, 5);
+        placeAsk(addressA, 213);
+        placeAsk(addressB, 2);
+        placeAsk(addressC, 5);
         System.out.println(asString());
 
-        answer(addressD,221);
+        answer(221);
         System.out.println(asString());
         System.out.println(findAllowedAmount(addressA));
         withdrawAll(addressA);
