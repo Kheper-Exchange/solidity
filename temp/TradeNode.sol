4878cf4033ca59694a475c7524a0faf4cadef879
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-01 19:21:38 -0400

    Update TradeNode.sol

--- temp/TradeNode.sol
+++ temp/TradeNode.sol
@@ -3,108 +3,102 @@
 pragma solidity >= 0.7 .0 < 0.9 .0;
 pragma abicoder v2; //remove for testing!
 
- 
-
-
 contract TradeNode {
-    struct Bid{
-        address bidderAddress;
-        uint amount;
-    }
-    
-    
-    uint private lowestId;
-    uint private currentId;
-    
-    mapping(uint => Bid) private statusMap;
-    mapping(address=>uint) private withdrawnAmount;
+  struct Bid {
+    address bidderAddress;
+    uint amount;
+  }
 
-    
-    function placeAsk(address usrAddress, uint amount) public returns(bool) {
-        require(currentId<10000 && amount>0);
-        statusMap[currentId].bidderAddress = usrAddress;
-        statusMap[currentId].amount = amount;
-        ++currentId;
-        
-        return true;
+  uint private lowestId;
+  uint private currentId;
+
+  mapping(uint => Bid) private statusMap;
+  mapping(address => uint) private withdrawnAmount;
+
+  function placeAsk(address usrAddress, uint amount) public returns(bool) {
+    require(currentId < 10000 && amount > 0);
+    statusMap[currentId].bidderAddress = usrAddress;
+    statusMap[currentId].amount = amount;
+    ++currentId;
+
+    return true;
+  }
+
+  function getTotalAmount() public view returns(uint) {
+    uint ret = 0;
+    for (uint y = lowestId; y < currentId; ++y) {
+      if (y == 10000) return 0;
+      ret += statusMap[y].amount;
     }
-  
-    function getTotalAmount() public view returns(uint) {
-        uint ret = 0;
-        for(uint y=lowestId;y<currentId;++y){
-            if(y==10000) return 0;
-        	ret += statusMap[y].amount;
-        }
-        return ret;
+    return ret;
+  }
+
+  function answer(uint amount) public returns(bool) {
+    require(amount > 0);
+    uint totalamt = getTotalAmount();
+    require(amount <= totalamt);
+    require((totalamt - amount) > (totalamt / 100));
+
+    if (amount == totalamt) {
+      lowestId = currentId;
+    } else {
+      uint res0 = findId(amount);
+      uint res1 = findAmount(amount);
+      require(res0 != 0 && res1 != 0);
+      lowestId = res0;
+      statusMap[res0].amount -= res1;
     }
-  
-    function answer(uint amount) public returns(bool) {
-        require(amount>0);
-        uint totalamt = getTotalAmount();
-        require(amount<=totalamt);
-        require((totalamt-amount)>(totalamt/100));
-        
-			if (amount == totalamt){
-				lowestId = currentId;
-			}
-			else {
-				uint res0 = findId(amount);
-				uint res1 = findAmount(amount);
-				require(res0!=0 && res1!=0);
-				lowestId = res0;
-				statusMap[res0].amount -= res1;
-			}
-	    return true;
+    return true;
+  }
+
+  function findId(uint amount) private view returns(uint) {
+    uint t = lowestId;
+    while (t < currentId) {
+      if (t == 10000) return 0; //not found
+      uint amtAt = statusMap[t].amount;
+      if (amtAt == amount)
+        return t++;
+      if (amtAt > amount)
+        return t;
+      amount -= amtAt;
+      ++t;
     }
-    
-    function findId(uint amount) private view returns(uint) {
-        uint t=lowestId;
-        while (t< currentId) {
-            if(t==10000) return 0; //not found
-			uint amtAt = statusMap[t].amount;
-			if (amtAt == amount)
-				return t++;
-			if (amtAt > amount)
-				return t;
-			amount -= amtAt;
-            ++t;
-        }
-        return 0; //not found
+    return 0; //not found
+  }
+
+  function findAmount(uint amount) private view returns(uint) {
+    uint t = lowestId;
+    while (t < currentId) {
+      if (t == 10000) return 0; //not found
+      uint amtAt = statusMap[t].amount;
+      if (amtAt == amount)
+        return amount;
+      if (amtAt > amount)
+        return amount;
+      amount -= amtAt;
+      ++t;
     }
-    
-    function findAmount(uint amount) private view returns(uint) {
-        uint t=lowestId;
-        while (t< currentId) {
-            if(t==10000) return 0; //not found
-            uint amtAt = statusMap[t].amount;
-            if (amtAt == amount)
-                return amount;
-            if (amtAt > amount)
-                return amount;
-            amount -= amtAt;
-            ++t;
-        }
-        return 0; //not found
+    return 0; //not found
+  }
+
+  function findAllowedAmount(address usrAddress) private view returns(uint) {
+    uint ret = 0;
+    uint t = 0;
+    while (t < lowestId) {
+      if (usrAddress == statusMap[t].bidderAddress)
+        ret += statusMap[t].amount;
+      ++t;
     }
-    
-    function findAllowedAmount(address usrAddress) private view returns(uint) {
-        uint ret = 0;
-        uint t=0;
-        while (t<lowestId) {
-            if (usrAddress==statusMap[t].bidderAddress)
-                ret += statusMap[t].amount;
-            ++t;
-        }
-        if(withdrawnAmount[usrAddress]!=0)
-        ret -= withdrawnAmount[usrAddress];
-        
-        return ret;
-    }
-    
-    function withdrawAll() public returns(bool) {
-        uint amt = findAllowedAmount(msg.sender);
-        if(amt!=0) withdrawnAmount[msg.sender] = withdrawnAmount[msg.sender]+ amt;
-        return true;
-    }
-    
+    if (withdrawnAmount[usrAddress] != 0)
+      ret -= withdrawnAmount[usrAddress];
+
+    return ret;
+  }
+
+  function withdrawAll() public returns(bool) {
+    uint amt = findAllowedAmount(msg.sender);
+    if (amt != 0) withdrawnAmount[msg.sender] = withdrawnAmount[msg.sender] + amt;
+    return true;
+  }
+
 }
