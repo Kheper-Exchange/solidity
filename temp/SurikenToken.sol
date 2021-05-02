79c1d334e6ef9ace4461cd1212d5d1270616beed
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-05-02 10:30:21 -0400

    Update SurikenToken.sol

--- temp/SurikenToken.sol
+++ temp/SurikenToken.sol
@@ -99,9 +99,9 @@ abstract contract NewToken {
 }
 
 contract TokenRegistration is Ownable {
-
-  //User can register a token using the owner account, if and only if they can make a call to "owner()" function.
-  //Otherwise, they have to contact the exchange owner for a listing.
+    
+//User can register a token using the owner account, if and only if they can make a call to "owner()" function.
+//Otherwise, they have to contact the exchange owner for a listing.
 
   mapping(address => string) private tokenName;
   mapping(address => string) private tokenSymbol;
@@ -110,74 +110,61 @@ contract TokenRegistration is Ownable {
   mapping(address => string) private socialURL;
   mapping(address => string) private whitePaperURL;
   mapping(address => string) private description;
-  mapping(address => uint) private flag1; //for internal use only, 100 is a block
-  mapping(address => uint) private flag2;
-  mapping(address => uint) private flag3;
+  mapping(address => uint) private flag1; //for internal use only, 10000 is a block
 
   function getContractOwner(address tokenAddress) private view returns(address) {
     NewToken newToken = NewToken(tokenAddress);
     return newToken.owner();
   }
-
   function setFlag1(address tokenAddress, uint flag) external onlyOwner returns(bool) {
     flag1[tokenAddress] = flag;
     return true;
   }
-
-  function setFlag2(address tokenAddress, uint flag) external onlyOwner returns(bool) {
-    flag2[tokenAddress] = flag;
-    return true;
-  }
-
-  function setFlag3(address tokenAddress, uint flag) external onlyOwner returns(bool) {
-    flag3[tokenAddress] = flag;
-    return true;
-  }
-
+  
   function changeTokenName(address tokenAddress, string memory tokenName_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     tokenName[tokenAddress] = tokenName_;
     return true;
   }
 
   function changeTokenSymbol(address tokenAddress, string memory tokenSymbol_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     tokenSymbol[tokenAddress] = tokenSymbol_;
     return true;
   }
 
   function changeTokenLogo(address tokenAddress, string memory logoURL_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     logoURL[tokenAddress] = logoURL_;
     return true;
   }
 
   function changeTokenWeb(address tokenAddress, string memory webURL_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     webURL[tokenAddress] = webURL_;
     return true;
   }
 
   function changeTokenSocial(address tokenAddress, string memory socialURL_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     socialURL[tokenAddress] = socialURL_;
     return true;
   }
 
   function changeTokenWhitepaper(address tokenAddress, string memory whitePaperURL_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     whitePaperURL[tokenAddress] = whitePaperURL_;
     return true;
   }
 
   function changeTokenDescription(address tokenAddress, string memory description_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     description[tokenAddress] = description_;
     return true;
   }
 
   function registerToken(address tokenAddress, string memory tokenName_, string memory tokenSymbol_, string memory logoURL_, string memory webURL_, string memory socialURL_, string memory whitePaperURL_, string memory description_) external returns(bool) {
-    require((flag1[msg.sender] != 100 && msg.sender == owner()) || msg.sender == getContractOwner(tokenAddress));
+    require((flag1[msg.sender]!=100 && msg.sender==owner()) || msg.sender == getContractOwner(tokenAddress));
     tokenName[tokenAddress] = tokenName_;
     tokenSymbol[tokenAddress] = tokenSymbol_;
     logoURL[tokenAddress] = logoURL_;
